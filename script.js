e020085c9b3c236553958766eed826edab15b84a
MissKluck <sigrid.b.kluck@gmail.com>
2024-05-31 13:15:02 +0200

    deleted images + playing with lists in js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
+  let currentImage;
   let slides = document.getElementsByClassName('mySlides');
   let dots = document.getElementsByClassName('dot');
   if (n > slides.length) {
@@ -21,14 +21,14 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+  for (currentImage = 0; currentImage < slides.length; currentImage++) {
+    slides[currentImage].style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
+  for (currentImage = 0; currentImage < dots.length; currentImage++) {
+    dots[currentImage].className = dots[currentImage].className.replace(' active', '');
   }
   slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
+  dots[slideIndex - 1].className += 'active';
 }
 
 /* Wire things up */
